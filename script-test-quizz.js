40b2ae881cf6d92461ae386ebb9bada4398f9c53
Martin DEBOUVILLE <debouvillemartin31@gmail.com>
2025-03-21 11:35:01 +0100

    J'ai ajouté un page test JS

---
+++ script-test-quizz.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector("#nest-button");
+const answers = document.querySelectorAll("main div");
+
+for (const answer of answers) {
+    answer.addEventListener("click", function () {
+
+
+    })
+}
